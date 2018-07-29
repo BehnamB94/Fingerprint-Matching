70396cba9cef9fab36d4c35d419f3c265d12d8e6
Behnam <b.bakhshi94@gmail.com>
2018-07-29 06:33:22 -0700

    fix reading dataset

--- run.py
+++ run.py
@@ -43,14 +43,14 @@ print_and_log('\n', ''.join(['#'] * 50),
 # PREPARE DATA
 #######################################################################################
 if args.Dataset == 'NIST':
-    loaded = np.load('dataset/fvc_normalize_181_181.npz')
+    loaded = np.load('dataset/images_181_181.npz')
     sample1 = loaded['sample1'].reshape((-1, 1, IMAGE_ROW, IMAGE_COL))
     sample2 = loaded['sample2'].reshape((-1, 1, IMAGE_ROW, IMAGE_COL))
     sample_list = [sample1, sample2]
     test_size = 400  # from 2000
     valid_size = 100  # from 2000
 else:  # FVC2002
-    loaded = np.load('dataset/fvc_181_181.npz')
+    loaded = np.load('dataset/fvc_normalize_181_181.npz')
     loaded = loaded['DB{}'.format(args.Dataset[-1])]
     sample_list = [loaded[:, i, :, :].reshape(-1, 1, IMAGE_ROW, IMAGE_COL) for i in range(8)]
     test_size = 10  # from 110
