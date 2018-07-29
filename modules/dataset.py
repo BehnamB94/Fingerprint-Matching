2aa7ca1505f1b407cd711e1fb4f6bbb0848b4c7a
behnam <b.bakhshi94@gmail.com>
2018-07-29 19:36:57 +0430

    change data loader to pre-process images

--- modules/dataset.py
+++ modules/dataset.py
@@ -1,13 +1,23 @@
+import numpy as np
 from torch.utils.data import Dataset
 
 
 class ImageDataset(Dataset):
-    def __init__(self, feature_arr, label_arr):
+    def __init__(self, feature_arr, label_arr, pre_process=True):
         self.feature_arr = feature_arr
         self.label_arr = label_arr
+        self.pre_process = pre_process
 
     def __len__(self):
         return self.feature_arr.shape[0]
 
     def __getitem__(self, idx):
-        return self.feature_arr[idx], self.label_arr[idx]
+        if self.pre_process:
+            feature = self.feature_arr[idx].astype(np.float)
+            feature[0] -= feature[0].min()
+            feature[0] /= feature[0].max()
+            feature[1] -= feature[1].min()
+            feature[1] /= feature[1].max()
+        else:
+            feature = self.feature_arr[idx]
+        return feature, self.label_arr[idx]
